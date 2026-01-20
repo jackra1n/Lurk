80b56485b00ffd5e60c050f33eeaf451d10bd748
jackra1n <45038833+jackra1n@users.noreply.github.com>
2026-01-20 22:37:31 +0100

    feat: add vite plugin for server-side startup
    because vite is lazy it doesnt start backend until its needed. this solves this problem

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    {
+      name: "lurk-startup",
+      configureServer(server) {
+        server.ssrLoadModule("/src/hooks.server.ts").catch((err) => {
+          console.error(
+            "[Vite Plugin] Failed to trigger backend initialization:",
+            err,
+          );
+        });
+      },
+    },
+  ],
 });
